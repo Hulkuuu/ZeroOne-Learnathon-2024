c4c972f886800c880f6dcb4b83543a4156c30ac5
Teja <163730659+Hulkuuu@users.noreply.github.com>
2024-10-18 13:03:02 +0530

    Delete basics directory

--- basics/add.js
+++
@@ -1,3 +0,0 @@
-const n=10;
-n="hulkuuu"; // error cause const will be constant through out the code in terms of data type
-console.log(n);
--- basics/info.js
+++
@@ -1,6 +0,0 @@
-const name  = "Teja";
-let id = 2300033461;
-var br = "Cse HTE";
-console.log(name);
-console.log(id);
-console.log(br);
